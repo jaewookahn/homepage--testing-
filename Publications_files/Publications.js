896e0cfc8eba62f5bb90ffe5e220cff5237cf568
jaewookahn <jaewook.ahn@gmail.com>
2010-02-14 23:20:27 +0000

    Homepage version 2010

---
+++ Publications_files/Publications.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.1 local-build-20100214
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Publications_files/PublicationsMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');fixAllIEPNGs('Media/transparent.gif');Widget.onload();performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
