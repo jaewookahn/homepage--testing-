b5ed4bc3c3ed46c1e7cc071c4259e906604ce09c
jaewookahn <jaewook.ahn@gmail.com>
2013-09-04 14:26:18 +0000

    fixed drexel ga code

--- homepage/VIBEFusion_files/VIBEFusion.js
+++ homepage/VIBEFusion_files/VIBEFusion.js
@@ -1,4 +1,4 @@
-// Created by iWeb 3.0.4 local-build-20130218
+// Created by iWeb 3.0.4 local-build-20130904
 
 setTransparentGifURL('Media/transparent.gif');function applyEffects()
 {var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWPhotoFrame([IWCreateImage('VIBEFusion_files/Watercolor_iweb_01.png'),IWCreateImage('VIBEFusion_files/Watercolor_iweb_02.png'),IWCreateImage('VIBEFusion_files/Watercolor_iweb_04.png'),IWCreateImage('VIBEFusion_files/Watercolor_iweb_08.png'),IWCreateImage('VIBEFusion_files/Watercolor_iweb_16.png'),IWCreateImage('VIBEFusion_files/Watercolor_iweb_14.png'),IWCreateImage('VIBEFusion_files/Watercolor_iweb_13.png'),IWCreateImage('VIBEFusion_files/Watercolor_iweb_05.png')],null,0,0.800000,0.000000,0.000000,0.000000,0.000000,12.000000,12.000000,11.000000,11.000000,13.000000,12.000000,13.000000,12.000000,null,null,null,0.500000)});registry.applyEffects();}
